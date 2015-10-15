8bfd65d24054f111377649c2c8924d7e02785878
Logan Sobczak <logansobczak@Laurens-iMac.local>
2015-10-15 19:30:20 -0400

    changed the directory structure, added a setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+	name="tasky",
+	version="0.0.1",
+	author="Logan Sobczak",
+	email="loganhsobczak@gmail.com",
+	py_modules=["Flask", "pymongo", "bcrypt", "PyJWT"]
+	)
